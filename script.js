84b67e3bdc5b8230f74b4ce516bdb8bb191afb61
sofydon <petinova@bk.ru>
2022-05-04 14:41:03 -0400

    click on third btn, first img hide

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ $(document).ready(function(){
     $('.list-item:first').hover(function() {
         $(this).toggleClass('active');
     });
+
+    $('.list-item:eq(2)').on('click', function(){
+        $('.image:even').fadeToggle('slow');
+    });
 });
